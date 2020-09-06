ba1d3cc223a7445dd1ecb7f496e778d5537a7d89
Kathryn Tucker <kathryn.tucker94@gmail.com>
2020-09-06 08:57:07 -0500

    Worked on the inheritance bookwork.

---
+++ src/org/launchcode/java/demos/lsn6inheritance/Main.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.demos.lsn6inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+
+        HouseCat spike = new HouseCat("Spike");
+        System.out.println(spike.getWeight());   // prints 13.0
+
+
+
+    }
+}
