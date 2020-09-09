179181d356085ff6f9a8722edfe9324c0c3b71fe
Kathryn Tucker <kathryn.tucker94@gmail.com>
2020-09-09 13:26:50 -0500

    Added technology package for the inheritance lesson.

---
+++ src/excercises/technology/Computer.java
@@ -0,0 +1,15 @@
+package excercises.technology;
+
+public class Computer {
+    private String name;
+    private String processor;
+    private String brand;
+
+    public Computer(String name, String processor, String brand) {
+        this.name = name;
+        this.processor = processor;
+        this.brand = brand;
+
+    }
+
+}
